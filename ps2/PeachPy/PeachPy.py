092849527abc68ab2086aabba4fd4718f404475d
pnvnd <peter@datacrunch.ca>
2021-05-16 17:33:26 -0400

    Updated GUI

--- ps2/PeachPy/PeachPy.py
+++ ps2/PeachPy/PeachPy.py
@@ -64,28 +64,16 @@ window = Tk()
 window.title("PeachPy - Tales of Rebirth (PS2) Tool")
 
 menubar = Menu(window)
+
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Close", command=donothing)
 
-filemenu.add_separator()
-
-filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Undo", command=donothing)
+filemenu.add_command(label="Change Work Directory", command= work_dir)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
 
-editmenu.add_separator()
-
-editmenu.add_command(label="Cut", command=donothing)
-editmenu.add_command(label="Copy", command=donothing)
-editmenu.add_command(label="Paste", command=donothing)
-editmenu.add_command(label="Delete", command=donothing)
-editmenu.add_command(label="Select All", command=donothing)
-
-menubar.add_cascade(label="Edit", menu=editmenu)
 helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="Help Index", command=donothing)
-helpmenu.add_command(label="About...", command=about)
+helpmenu.add_command(label="About", command=about)
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 window.config(menu=menubar)
@@ -103,64 +91,64 @@ label.grid(row=0, column=0, columnspan=4)
 frame1 = LabelFrame(window, text="Unpack", padx=5, pady=5)
 frame1.grid(row=1, column=0, padx=10, pady=10)
 
-btn_unpackFPB = Button(frame1, text="Unpack FPB", command = extract_dat)
+btn_unpackFPB = Button(frame1, text="Unpack DAT", command = extract_dat)
 btn_unpackFPB.grid(row=0, column=0)
 
 btn_unpackSCPK = Button(frame1, text="Unpack SCPK", command = extract_scpk)
 btn_unpackSCPK.grid(row=1, column=0)
 
-btn_unpackSCED = Button(frame1, text="Unpack SCED", command = extract_sced)
-btn_unpackSCED.grid(row=2, column=0)
+btn_THEIRSCE = Button(frame1, text="Unpack THEIRSCE", command = extract_theirsce)
+btn_THEIRSCE.grid(row=2, column=0)
 
 btn_unpackPAK1 = Button(frame1, text="Unpack PAK1", command = extract_pak1)
 btn_unpackPAK1.grid(row=3, column=0)
 
-btn_moveOUT = Button(frame1, text="Move Skits OUT", command = move_skits_out)
-btn_moveOUT.grid(row=4, column=0)
+btn_unpackPAK2 = Button(frame1, text="Unpack PAK2", command = extract_pak2_theirsce)
+btn_unpackPAK2.grid(row=4, column=0)
 
-btn_unpackSKIT = Button(frame1, text="Extract SKIT", command = extract_skit)
-btn_unpackSKIT.grid(row=5, column=0)
+btn_unpackMFH = Button(frame1, text="Unpack MFH", command = extract_mfh)
+btn_unpackMFH.grid(row=5, column=0)
 
 frame2 = LabelFrame(window, text="Re-pack", padx=5, pady=5)
 frame2.grid(row=1, column=1, padx=10, pady=10)
 
-btn_packFPB = Button(frame2, text="Pack FPB", command = pack_dat)
-btn_packFPB.grid(row=0, column=0)
+btn_packDAT = Button(frame2, text="Pack DAT", command = pack_dat)
+btn_packDAT.grid(row=0, column=0)
 
 btn_packSCPK = Button(frame2, text="Pack SCPK", command = pack_scpk)
 btn_packSCPK.grid(row=1, column=0)
 
-btn_packSCED = Button(frame2, text="Pack SCED", command = insert_sced)
-btn_packSCED.grid(row=2, column=0)
+btn_packTHEIRSCE = Button(frame2, text="Pack THEIRSCE", command = insert_theirsce)
+btn_packTHEIRSCE.grid(row=2, column=0)
 
-btn_packPAK1 = Button(frame2, text="Pack PAK1", command = insert_pak1)
+btn_packPAK1 = Button(frame2, text="Pack PAK1", command = donothing)
 btn_packPAK1.grid(row=3, column=0)
 
-btn_moveIN = Button(frame2, text="Move Skits IN", command = move_skits_in)
-btn_moveIN.grid(row=4, column=0)
+btn_packPAK2 = Button(frame2, text="Pack PAK2", command = donothing)
+btn_packPAK2.grid(row=4, column=0)
 
-btn_packSKIT = Button(frame2, text="Insert SKIT", command = insert_skit)
-btn_packSKIT.grid(row=5, column=0)
+btn_packMFH = Button(frame2, text="Pack MFH", command = donothing)
+btn_packMFH.grid(row=5, column=0)
 
 frame3 = LabelFrame(window, text="Misc.", padx=5, pady=5)
 frame3.grid(row=1, column=2, padx=10, pady=10)
 
-btn_sortFPB = Button(frame3, text="Organize FPB", command = unpack)
+btn_sortFPB = Button(frame3, text="Sort DAT", command = unpack)
 btn_sortFPB.grid(row=0, column=0)
 
-btn_unpackMOVIE = Button(frame3, text="Unpack MOVIE", command = extract_movie)
+btn_unpackMOVIE = Button(frame3, text="Unpack MOVIE", command = extract_mov)
 btn_unpackMOVIE.grid(row=1, column=0)
 
-btn_insertFONT = Button(frame3, text="Insert FONT", command = insert_font)
-btn_insertFONT.grid(row=2, column=0)
+btn_extractTMSK = Button(frame3, text="Extract TMSK", command = extract_tmsk)
+btn_extractTMSK.grid(row=2, column=0)
 
-btn_exportTBL = Button(frame3, text="Export TBL", command = export_tbl)
-btn_exportTBL.grid(row=3, column=0)
+btn_extractTMRC = Button(frame3, text="Extract TMRC", command = extract_tmrc)
+btn_extractTMRC.grid(row=3, column=0)
 
-btn_pak1skit = Button(frame3, text="Pack Movie (Broken)")
-btn_pak1skit.grid(row=4, column=0)
+btn_pakMOV = Button(frame3, text="Pack Movie (Broken)", command = donothing)
+btn_pakMOV.grid(row=4, column=0)
 
-btn_getPWD = Button(frame3, text="Change Work Directory", command = work_dir)
+btn_getPWD = Button(frame3, text="Export TBL", command = export_tbl)
 btn_getPWD.grid(row=5, column=0)
 
 #Set working directory for GUI
