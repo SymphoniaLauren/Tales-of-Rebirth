ac206851a8a5af499a041a8e18d9d2e238be7d3a
Mc-muffin <8714476+Mc-muffin@users.noreply.github.com>
2024-12-30 06:05:24 -0500

    Rework title screen text
    Just a bit

--- tools/hacks/src/Externs.h
+++ tools/hacks/src/Externs.h
@@ -4,7 +4,7 @@
 extern "C"
 {
 	void fntenv_make_default(fontenv_struct* fntenv);
-	void get_str_width(fontenv_struct* fntenv, const char str[], int width, int height);
+	void get_str_width(fontenv_struct* fntenv, const char str[], u16* width, u16* height);
 	void draw_string(fontenv_struct* fntenv, const char str[]);
 	int getBtlVoiceTimer(btl_chr_struct* btl_chr, int voice_id);
 	int sprintf ( char * str, const char * format, ... );
